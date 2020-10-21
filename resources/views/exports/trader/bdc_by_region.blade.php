446bd8ea06cc60a1479e92ea2172cbf9dd668509
devbertram <bertram.langruto@gmail.com>
2020-10-21 15:16:44 +0800

    Trader List By Date per Region

--- resources/views/exports/trader/bdc_by_region.blade.php
+++ resources/views/exports/trader/bdc_by_region.blade.php
@@ -66,24 +66,32 @@
           </tr>
 
           @foreach($trader_registrations as $key => $tr_data)
+            
+            @if (!empty($tr_data->trader))
+              
+              @if ($tr_data->trader->region_id == $region_data->region_id)
 
-            <tr>
-              <td style="width:3px; text-align: center; vertical-align: top; font-size: 11px;">
-                {{ $i += 1 }}
-              </td>
-              <td style="width:45px; font-size: 11px;">
-                {{ optional($tr_data->trader)->name }}<br>
-                {{ optional($tr_data->trader)->address }}<br>
-                {{ $tr_data->trader_officer }}<br>
-                {{ $tr_data->trader_email }}<br>
-              </td>
-              <td style="width:20px; text-align: center; vertical-align: top; font-size: 11px;">
-                  {{ optional($tr_data->trader)->tin }}
-              </td>
-              <td style="width:20px; text-align: center; vertical-align: top; font-size: 11px;">
-                  {{ optional($tr_data->trader)->tel_no }}
-              </td>
-            </tr>
+                <tr>
+                  <td style="width:3px; text-align: center; vertical-align: top; font-size: 11px;">
+                    {{ $i += 1 }}
+                  </td>
+                  <td style="width:45px; font-size: 11px;">
+                    {{ optional($tr_data->trader)->name }}<br>
+                    {{ optional($tr_data->trader)->address }}<br>
+                    {{ $tr_data->trader_officer }}<br>
+                    {{ $tr_data->trader_email }}<br>
+                  </td>
+                  <td style="width:20px; text-align: center; vertical-align: top; font-size: 11px;">
+                      {{ optional($tr_data->trader)->tin }}
+                  </td>
+                  <td style="width:20px; text-align: center; vertical-align: top; font-size: 11px;">
+                      {{ optional($tr_data->trader)->tel_no }}
+                  </td>
+                </tr>
+
+              @endif
+
+            @endif
 
           @endforeach
 
